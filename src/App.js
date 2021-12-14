329d683c271f956eedc0bb3628c9f1977992d124
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-12-14 09:57:39 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from 'react-redux'
+import { incValue, decValue, setValue } from './redux/actions/actions'
 
-function App() {
+import './App.css'
+
+
+const App = ({ countValue, onIncreaseValue, onDecreaseValue, onSetCountValue }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div>{countValue}</div>
+      <div className="pt30">
+        <input 
+          onChange={(event) => onSetCountValue(event.target.value)} type="text" />
+        <button onClick={() => onDecreaseValue()}>Decrement</button>
+        <button onClick={() => onIncreaseValue()}>Increment</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countValue: state.countValue
+})
+const mapDispathToProps = (dispatch) => ({
+  onSetCountValue: (value) => dispatch(setValue(value)),
+  onIncreaseValue: () => dispatch(incValue()),
+  onDecreaseValue: () => dispatch(decValue())
+})
+export default connect(mapStateToProps, mapDispathToProps)(App)
